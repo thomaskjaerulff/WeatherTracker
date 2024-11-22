7ed6b064876c6c4704e251402a60d02ba5d7fc3c
Thomas Kjærulff <thomas.kjaerulff@fellowmind.dk>
2024-11-22 17:22:12 +0100

    Add textbox to input city.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div>
+      Enter a city here: <input type="text" value={inputValue} onChange={handleInputChange} />
+      <p>You entered: {inputValue}</p>
+    </div>
+  );
 }
 
 export default App;
